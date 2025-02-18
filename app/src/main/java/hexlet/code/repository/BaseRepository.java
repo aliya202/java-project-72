d9c59c248301595e271b0ec675ac14e677c967f1
Aliya <aakhisamova@gmail.com>
2025-02-19 01:48:40 +0400

    delete data source provider

--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -1,8 +1,7 @@
 package hexlet.code.repository;
 
 import com.zaxxer.hikari.HikariDataSource;
-import hexlet.code.database.DataSourceProvider;
 
 public class BaseRepository {
-    public static HikariDataSource dataSource = (HikariDataSource) DataSourceProvider.getDataSource();
+    public static HikariDataSource dataSource;
 }
