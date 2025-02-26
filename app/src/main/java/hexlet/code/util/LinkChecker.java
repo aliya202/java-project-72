fa84c2086915cb5d7ba73f26f1aaf541354c4bde
Aliya <aakhisamova@gmail.com>
2025-02-27 01:09:54 +0400

    change pattern

--- app/src/main/java/hexlet/code/util/LinkChecker.java
+++ app/src/main/java/hexlet/code/util/LinkChecker.java
@@ -7,16 +7,17 @@ public class LinkChecker {
 
     public static boolean isLinkValid(String text) throws Exception {
         Pattern pattern = Pattern.compile(
-                "^(https?://)" +
-                        "(?:" +
-                        "(localhost(?::\\d{1,5})?)" +
-                        "|" +
-                        "((?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+" +
-                        "(?:ru|com|org|net|edu|gov|io)" +
-                        "(?::\\d{1,5})?" +
-                        ")" +
-                        ")" +
-                        "(/.*)?$");
+                "^(https?://)"
+                        + "(?:"
+                        + "(localhost(?::\\d{1,5})?)"
+                        + "|"
+                        + "((?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+"
+                        + "(?:ru|com|org|net|edu|gov|io)"
+                        + "(?::\\d{1,5})?"
+                        + ")"
+                        + ")"
+                        + "(/.*)?$"
+        );
         Matcher matcher = pattern.matcher(text);
 
         if (!matcher.find()) {
