74e16575f54891240d52bbf7d231f0807fa9d84f
Aliya <aakhisamova@gmail.com>
2025-02-27 00:59:20 +0400

    change pattern

--- app/src/main/java/hexlet/code/util/LinkChecker.java
+++ app/src/main/java/hexlet/code/util/LinkChecker.java
@@ -1,31 +1,26 @@
 package hexlet.code.util;
 
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LinkChecker {
 
-    private static final Set<String> VALID_TLDS = Set.of(
-            "com", "org", "net", "edu", "gov", "mil", "int", "ru", "us", "uk", "fr", "de", "cn",
-            "br", "in", "jp", "kr", "ca", "au", "it", "es", "nl", "se", "no", "fi", "dk", "io"
-    );
-
     public static boolean isLinkValid(String text) throws Exception {
-
         Pattern pattern = Pattern.compile(
-                "^(https?://(?:[\\w-]+\\.)+([a-zA-Z]{2,6})|localhost)(?::\\d{1,5})?(?:/.*)?$",
-                Pattern.CASE_INSENSITIVE
-        );
+                "^(https?://)" +
+                        "(?:" +
+                        "(localhost(?::\\d{1,5})?)" +
+                        "|" +
+                        "((?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+" +
+                        "(?:ru|com|org|net|edu|gov|io)" +
+                        "(?::\\d{1,5})?" +
+                        ")" +
+                        ")" +
+                        "(/.*)?$");
         Matcher matcher = pattern.matcher(text);
 
         if (!matcher.find()) {
             throw new Exception("Некорректный URL");
-        }
-
-        String tld = matcher.group(2);
-        if (tld == null || !VALID_TLDS.contains(tld.toLowerCase())) {
-            throw new Exception("Некорректный домен");
         }
 
         return true;
